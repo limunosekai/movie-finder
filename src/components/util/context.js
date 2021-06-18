57ed376a0a74b52c2657b05136fec8afa3e3aa85
limunosekai <powerlsh0103@gmail.com>
2021-06-18 18:34:22 +0900

    Add README.md

--- src/components/util/context.js
+++ src/components/util/context.js
@@ -15,7 +15,7 @@ const AppProvider = ({ children }) => {
     try {
       const response = await fetch(url);
       const data = await response.json();
-
+      console.log(data);
       if (data.Response === 'True') {
         setMovies(data.Search);
         setError({ show: false, msg: '' });
